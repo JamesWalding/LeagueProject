7848a12fb8261cc39b7ac87f31dc97108531ab02
James Walding <jamez11454@hotmail.co.uk>
2021-09-08 19:23:16 +0100

    got the basis of the api pulling in information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LeagueAngular';
+  constructor(private http: HttpClient) {
+    this.loadProfile();
+  }
+  loadProfile(){
+    this.http.get('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/suitednboosted?api_key=RGAPI-35dc4fb9-53df-44db-a867-f0b08c0dc060').subscribe((response)=>{
+      alert(JSON.stringify(response));
+    });
+  }
 }
